934bd2030989243d3a2cbe6ef178ae6ca2d7079d
karivas-dev <72422927+karivas-dev@users.noreply.github.com>
2024-07-17 16:09:00 -0600

    Initial commit

---
+++ DES_Persona/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DES_Persona.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ DES_Persona/Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿namespace DES_Persona.Models
+{
+    public class Persona
+    {
+        //DUI, Nombre, Apellido, Fecha de nacimiento, Dirección y correo electrónico
+        public int id { get; set; }
+        public string dui { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public DateTime fechaNac {  get; set; }
+        public string direccion {  get; set; }
+        public string correo {  get; set; }
+    }
+}
